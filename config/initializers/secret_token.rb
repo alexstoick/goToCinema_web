c666bca2715ee1390b415d74b977a5ffb3d68f49
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-05 08:43:09 +0300

    initial commit. blank rails project + db settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CinemaWeb::Application.config.secret_token = 'de4e2600763ee09dd1448d336348c9613d1fc78f59f9d74197d7226e0ffebf149a9cd604f2773d45f28cce72239ba531423c9a219e8eb8f13c2efea1c247af7e'
