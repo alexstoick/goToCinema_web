9e83809a814e27ed1358a4e2c05df5851e231659
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-14 21:51:34 +0300

    changed database to cinemaAPI will start wall&posts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class SessionsController < ApplicationController
 
 	include BCrypt
@@ -9,13 +11,19 @@ class SessionsController < ApplicationController
 		user = User.find_by_username( params[:username] )
 		if ( user && user.authenticate( params[:password] ) )
 			session[:user_id] = user.id
-			render json: { "loggedIn" => true , "user_id" => user.id , "key" => Password.create( params[:username]+params[:password] ) }
+			key = Password.create( params[:username]+params[:password] )
+			user.authToken = key
+			user.save!
+			redirect_to root_path, notice: 'Successfully logged in!'
 		else
 			render json: { "loggedIn" => false }
 		end
 	end
 
 	def destroy
+		user = User.find_by_id ( session[:user_id] )
+		user.authToken = nil
+		user.save!
 		reset_session
 		redirect_to root_path
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-	attr_accessible :email, :password, :username
+
+	attr_accessible :email, :password, :username, :auth_token, :nume, :prenume
+
+	has_many :posts, :class_name => "Post", :foreign_key => "sender_id"
+	has_many :wall_posts, :class_name => "Post", :foreign_key => "receiver_id"
 
 	has_secure_password
+
 end
